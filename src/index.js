491d7ec03bf139bc401c459cb727a2128d359d5f
rbdiwash <rbdiwash@gmail.com>
2025-05-28 09:46:49 +1000

    importing routes fixed in index

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const connectDB = require("../src/config/db");
-const authRoutes = require("../src/routes/authRoutes");
-const noticeRoutes = require("../src/routes/notices");
-const userRoutes = require("../src/routes/user");
-const notificationRoutes = require("../src/routes/notifications");
-const maintenanceRoutes = require("../src/routes/maintenance");
-const leaseAgreementRoutes = require("../src/routes/leaseAgreement");
-const messageRoutes = require("../src/routes/messages");
-const tenantRoutes = require("../src/routes/tenants");
-const profileRoutes = require("../src/routes/profile");
-const propertyRoutes = require("../src/routes/property");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const noticeRoutes = require("./routes/notices");
+const userRoutes = require("./routes/user");
+const notificationRoutes = require("./routes/notifications");
+const maintenanceRoutes = require("./routes/maintenance");
+const leaseAgreementRoutes = require("./routes/leaseAgreement");
+const messageRoutes = require("./routes/messages");
+const tenantRoutes = require("./routes/tenants");
+const profileRoutes = require("./routes/profile");
+const propertyRoutes = require("./routes/property");
 dotenv.config();
 connectDB();
 
--- src/routes/notifications.js
+++ src/routes/notifications.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const Notification = require("../models/Notification");
 const router = express.Router();
+const Notification = require("../models/Notification");
 
 // Send Buzz
 router.post("/send", async (req, res) => {
